acaaa694cf74714321dd72d76a615b3bb48ee91b
Mohamed Haddad <mohamed.haddad@check24.de>
2022-04-29 10:06:12 +0200

    Initial Commit

---
+++ MapSUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapSUI
+//
+//  Created by Mohamed Haddad on 29.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapSUI/MapSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapSUIApp.swift
+//  MapSUI
+//
+//  Created by Mohamed Haddad on 29.04.22.
+//
+
+import SwiftUI
+
+@main
+struct MapSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
